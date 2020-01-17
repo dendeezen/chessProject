7a6252ee02d3924a9fccc32197440fe5433a2dbd
dendeezen <arno_vde@hotmail.com>
2020-01-17 12:50:48 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
+import { AppRoutingModule } from './app.routes';
+import { InitialComponent } from './initial/initial.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SpotifyComponent } from './spotify/spotify.component';
+import { SpotifyCallbackComponent } from './spotify-callback/spotify-callback.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import { ChessComponent } from './chess/chess.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { ChessPieceComponent } from './chess/chess-piece/chess-piece.component';
+import { GridComponentComponent } from './chess/grid-component/grid-component.component';
+import { GridListComponentComponent } from './chess/grid-list-component/grid-list-component.component';
+import {AngularDraggableModule} from 'angular2-draggable';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MycomponentComponent,
+    InitialComponent,
+    PageNotFoundComponent,
+    SpotifyComponent,
+    SpotifyCallbackComponent,
+    TictactoeComponent,
+    ChessComponent,
+    ChessPieceComponent,
+    GridComponentComponent,
+    GridListComponentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    FormsModule,
+    DragDropModule,
+    AngularDraggableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chess/coordinate.ts
@@ -0,0 +1,7 @@
+export class Coordinate {
+  constructor(s1: string) {
+    this.coordinate = s1;
+  }
+
+  coordinate: string;
+}
